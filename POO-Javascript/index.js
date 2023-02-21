6d4773b414c0703653c3f0fedb048c4bb98bc07f
f2lpOliveira <contato@fellipefreire.com.br>
2023-02-21 10:17:17 -0300

    Primeiro Commit!

---
+++ POO-Javascript/index.js
@@ -0,0 +1,13 @@
+const cliente1Nome = "Barney";
+const cliente1CPF = 11100011100;
+const cliente1Agencia = 0001;
+const cliente1Conta = 1100110;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Lola";
+const cliente2CPF = 11111100011;
+const cliente2Agencia = 0001;
+const cliente2Conta = 1111001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
